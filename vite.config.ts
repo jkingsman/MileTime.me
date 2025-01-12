7902a2b066dcf9226b9374e8f12f6db0e28bec32
jkingsman <jack.kingsman@gmail.com>
2025-01-11 22:03:18 -0800

    Add service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    // add this to cache all the imports
+    workbox: {
+      globPatterns: ["**/*"],
+    },
+    // add this to cache all the
+    // static assets in the public folder
+    includeAssets: [
+      "**/*",
+    ],
+    manifest: {
+      "name": "MileTime.me",
+      "short_name": "MileTime.me",
+      "scope": "/",
+      "start_url": "/",
+      "icons": [
+        {
+          "src": "/web-app-manifest-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "maskable"
+        },
+        {
+          "src": "/web-app-manifest-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ],
+      "theme_color": "#ffffff",
+      "background_color": "#ffffff",
+      "display": "standalone"
+    }
+  })],
 })
